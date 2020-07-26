b58c9d6dcdaab1a47181316e92fa06b9c9d24161
joilsonLeal <joilson_rg@hotmail.com>
2020-07-26 11:18:08 -0300

    feature: adiciona funcionalidade de salvar produtos no carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+import { connect } from 'react-redux';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete }
+  from 'react-icons/md';
 
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart() {
+function Cart({ cart }) {
   return (
     <Container>
       <ProductTable>
@@ -17,23 +23,24 @@ function Cart() {
           </tr>
         </thead>
         <tbody>
-          <tr>
+          { cart.map(product => (
+            <tr>
             <td>
               <img
-                src="https://static.netshoes.com.br/produtos/tenis-venetto-casual-caminhada-leve-confortavel-masculino/06/EPL-0186-006/EPL-0186-006_zoom2.jpg?ts=1564484531&ims=326x"
-                alt="Tênis"
+                src={product.image}
+                alt={product.title}
               />
             </td>
             <td>
-              <strong>Tênis muito massa</strong>
-              <span>R$ 129,90</span>
+              <strong>{product.title}</strong>
+              <span>{product.priceFormatted}</span>
             </td>
             <td>
               <div>
                 <button type="button">
                   <MdRemoveCircleOutline size={20} color="#7159C1" />
                 </button>
-                <input type="number" readOnly value={2} />
+                <input type="number" readOnly value={product.amount} />
                 <button type="button">
                   <MdAddCircleOutline size={20} color="#7159C1" />
                 </button>
@@ -48,6 +55,7 @@ function Cart() {
               </button>
             </td>
           </tr>
+          )) }
         </tbody>
       </ProductTable>
 
@@ -64,4 +72,8 @@ function Cart() {
   );
 }
 
-export default Cart;
+const mapStateToProps = state => ({
+  cart: state.cart,
+});
+
+export default connect(mapStateToProps)(Cart);
