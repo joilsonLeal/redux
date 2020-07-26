8ec621654191ca58c0e15ac75789378526a38829
joilsonLeal <joilson_rg@hotmail.com>
2020-07-26 11:24:43 -0300

    feature: remove produto do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -9,7 +9,7 @@ import {
 import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart }) {
+function Cart({ cart, dispatch }) {
   return (
     <Container>
       <ProductTable>
@@ -50,7 +50,11 @@ function Cart({ cart }) {
               <strong>R$ 258,80</strong>
             </td>
             <td>
-              <button type="button">
+              <button type="button"
+                onClick={() =>
+                  dispatch({ type: 'REMOVE_FROM_CART', id: product.id })
+                }
+              >
                 <MdDelete size={20} color="#7159C1" />
               </button>
             </td>
